3917f562cf1dba2f3f431a82ba3d96282a91220e
Tom Kaitchuck <tom.kaitchuck@emc.com>
2020-05-13 20:32:18 -0700

    Switch throttling to be done by FlowHandler once per connection rather than per flow.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- test/integration/src/test/java/io/pravega/test/integration/AppendTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/AppendTest.java
@@ -46,6 +46,7 @@ import io.pravega.segmentstore.server.host.handler.PravegaRequestProcessor;
 import io.pravega.segmentstore.server.host.handler.ServerConnectionInboundHandler;
 import io.pravega.segmentstore.server.store.ServiceBuilder;
 import io.pravega.segmentstore.server.store.ServiceBuilderConfig;
+import io.pravega.shared.metrics.MetricNotifier;
 import io.pravega.shared.protocol.netty.Append;
 import io.pravega.shared.protocol.netty.AppendDecoder;
 import io.pravega.shared.protocol.netty.CommandDecoder;
@@ -216,7 +217,7 @@ public class AppendTest {
     static EmbeddedChannel createChannel(StreamSegmentStore store) {
         ServerConnectionInboundHandler lsh = new ServerConnectionInboundHandler();
         EmbeddedChannel channel = new EmbeddedChannel(new ExceptionLoggingHandler(""),
-                new CommandEncoder(null),
+                new CommandEncoder(null, MetricNotifier.NO_OP_METRIC_NOTIFIER, () -> {}),
                 new LengthFieldBasedFrameDecoder(MAX_WIRECOMMAND_SIZE, 4, 4),
                 new CommandDecoder(),
                 new AppendDecoder(),
