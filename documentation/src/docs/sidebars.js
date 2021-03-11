63b2cb8cb438cff4cc9f677f57d93d7a50f75e58
Claudio Fahey <claudio.fahey@dell.com>
2021-03-11 00:15:05 +0000

    Update sidebar

--- documentation/src/docs/sidebars.js
+++ documentation/src/docs/sidebars.js
@@ -5,9 +5,48 @@ module.exports = {
         {
             'Understanding Pravega': [
                 'pravega-concepts',
-                'terminology'
+                'terminology',
+                'key-features',
+                'faq',
+                'segment-store-service',
+                'segment-containers',
+                'controller-service',
+                'wire-protocol',
+                'state-synchronizer-design',
+                'reader-group-design',
+                'watermarking',
+            ],
+            'Developing Pravega Applications': [
+                'javadoc',
+                'rest/restapis',
+                'connectors',
+                'basic-reader-and-writer',
+                'state-synchronizer',
+                'transactions',
+                'streamcuts',
+            ],
+            'Running Pravega': [
+                'deployment/deployment',
+                'deployment/manual-install',
+                'deployment/kubernetes-install',
+                'deployment/docker-swarm',
+                'deployment/dcos-install',
+                'deployment/aws-install',
+                'metrics',
+            ],
+            'Pravega Security': [
+                'auth/auth-plugin',
+                'auth/client-auth',
+                'security/pravega-security-authorization-authentication',
+                'security/pravega-security-configurations',
+                'security/pravega-security-encryption',
+                'security/securing-distributed-mode-cluster',
+            ],
+            'Contributing to Pravega': [
+                'contributing',
+                'roadmap',
+                'join-community',
             ]
         },
-        'faq',
     ]
 };
